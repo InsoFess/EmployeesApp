a304c86c5dddf5d00aed658b83a66d395d99165d
insofess <insofess@gmail.com>
2022-08-05 15:39:16 +0300

    final version

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
                 {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
             ],
-            term: ''
+            term: '', 
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -30,6 +31,7 @@ class App extends Component {
         })
     }
 
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
     addItem = (name, salary) => {
         const newItem = {
             name, 
@@ -56,39 +58,41 @@ class App extends Component {
             })
         }))
     }
-    
+
     searchEmp = (items, term) => {
         if (term.length === 0) {
-          return items;
+            return items;
         }
 
         return items.filter(item => {
-            return item.name.indexOf(term) > -1;
+            return item.name.indexOf(term) > -1
         })
     }
 
     onUpdateSearch = (term) => {
-        this.setState({term})
+        this.setState({term});
     }
 
-    onPromotion = (items) => {
-         console.log('Promotion')
-         return items.filter(item => {
-            return item.rise === true
-         })
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
     }
 
-    onSalary = (items) => {
-        return items.filter(item => {
-            return item.salary > 1000})
-
+    onFilterSelect = (filter) => {
+        this.setState({filter});
     }
 
     render() {
-        const {data,term} = this.state;
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data, term);
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
         return (
             <div className="app">
@@ -96,9 +100,7 @@ class App extends Component {
     
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter
-                    onPromotion={this.onPromotion}
-                    onSalary={this.onSalary}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 
                 <EmployeesList 
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,19 +1,18 @@
-import { Component } from 'react/cjs/react.production.min';
+import { Component } from 'react';
 import './search-panel.css';
 
 class SearchPanel extends Component {
     constructor(props) {
-        super(props)
-        this.state =  {
+        super(props);
+        this.state = {
             term: ''
         }
     }
 
     onUpdateSearch = (e) => {
         const term = e.target.value;
-        this.setState({term})
-        this.props.onUpdateSearch(term);
-
+        this.setState({term});
+        this.props.onUpdateSearch(term)
     }
 
     render() {
